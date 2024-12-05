71726199210b4a7dea74721bead7e1328d8fac71
jatin856989 <jatinbansal1936@gmail.com>
2024-12-05 17:10:33 +0530

    Updated resume and Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -53,19 +53,122 @@ const Navbar = () => {
         <ul className='list-none hidden sm:flex flex-row gap-10'>
 
 
-        <li
+        {/* <li
           className={`cursor-pointer hover:bg-[#915eff]/50 hover:shadow-md hover: text-white transition duration-600  ${
             active === "Resume" ? "text-white" : "text-secondary"
           }`}
         >
-          <a
+          <a```javascript
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { menu, close } from "../assets";
+import newLogo from "../assets/newlogo.svg";
+
+const Navbar = () => {
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      if (scrollTop > 100) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
+  const handleSetActive = (title) => {
+    setActive(title);
+  };
+
+  return (
+    <nav
+      className={`${
+        styles.paddingX
+      } w-full flex items-center py-5 fixed top-0 z-20 ${
+        scrolled ? "bg-primary" : "bg-transparent"
+      }`}
+    >
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        <Link
+          to='/'
+          className='flex items-center gap-2'
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={newLogo} alt='logo' className='w-9 h-9 object-contain' />
+          <p className='text-white text-[18px] font-bold cursor-pointer flex '>
+            Jatin &nbsp;
+            <span className='sm:block hidden'> | Developer</span>
+          </p>
+        </Link>
+
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((nav) => (
+            <li
+              key={nav.id}
+              className={`${
+                active === nav.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => handleSetActive(nav.title)}
+            >
+              <a href={`#${nav.id}`}>{nav.title}</a>
+            </li>
+          ))}
+        </ul>
+
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img
+            src={toggle ? close : menu}
+            alt='menu'
+            className='w-[28px] h-[28px] object-contain'
+            onClick={handleToggle}
+          />
+
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
+              {navLinks.map((nav) => (
+                <li
+                  key={nav.id}
+                  className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                    active === nav.title ? "text-white" : "text-secondary"
+                  }`}
+                  onClick={() => {
+                    handleToggle();
+                    handleSetActive(nav.title);
+                  }}
+                >
+                  <a href={`#${nav.id}`}>{nav.title}</a>
+                </li>
+              ))}
+            </ul
             href='/public/jatin_resume.pdf'
             download='Jatin Resume'
             className='text-white text-[18px] font-bold'
           >
             Resume
           </a>
-        </li>
+        </li> */}
 
 
 
